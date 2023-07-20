85edf0718b1096a84fa31335ec2e0f51cb642806
tungbingbong <hung2522001@gmail.com>
2023-07-20 17:17:45 +0700

    Build sidebar UI part #1

---
+++ src/layouts/components/Sidebar/Sidebar.js
@@ -0,0 +1,40 @@
+import classNames from 'classnames/bind';
+import styles from './Sidebar.module.scss';
+import Menu, { MenuItem } from './Menu';
+import {
+    HomeIcon,
+    UserGroupIcon,
+    ExploreIcon,
+    VideoIcon,
+    HomeActiveIcon,
+    UserGroupActiveIcon,
+    ExploreActiveIcon,
+    VideoActiveIcon,
+} from '~/components/Icons';
+import config from '~/config';
+
+const cx = classNames.bind(styles);
+
+function Sidebar() {
+    return (
+        <aside className={cx('wrapper')}>
+            <Menu>
+                <MenuItem title="For You" to={config.routes.home} icon={<HomeIcon />} activeIcon={<HomeActiveIcon />} />
+                <MenuItem
+                    title="Following"
+                    to={config.routes.following}
+                    icon={<UserGroupIcon />}
+                    activeIcon={<UserGroupActiveIcon />}
+                />
+                <MenuItem
+                    title="Explore"
+                    to={config.routes.explore}
+                    icon={<ExploreIcon />}
+                    activeIcon={<ExploreActiveIcon />}
+                />
+                <MenuItem title="LIVE" to={config.routes.live} icon={<VideoIcon />} activeIcon={<VideoActiveIcon />} />
+            </Menu>
+        </aside>
+    );
+}
+export default Sidebar;

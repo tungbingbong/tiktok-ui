6b9dd4e43ffe847477128e61756c84b959c56358
tungbingbong <hung2522001@gmail.com>
2023-08-25 01:04:06 +0700

    Implement infinity load

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,11 @@
-import { useContext, useEffect, useState } from 'react';
-import Video from '~/layouts/components/Video';
-import * as timelineService from '~/services/timelineService';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useCallback, useContext, useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
+
 import styles from './Home.module.scss';
 import { AuthUserContext } from '~/App';
+import Video from '~/layouts/components/Video';
+import * as timelineService from '~/services/timelineService';
 
 const cx = classNames.bind(styles);
 
@@ -16,23 +18,31 @@ function HomePage() {
     const authUser = useContext(AuthUserContext);
     const accessToken = authUser && authUser.meta.token ? authUser.meta.token : '';
 
-    useEffect(() => {
-        timelineService
-            .getVideos({ type: 'for-you', page: page, accessToken: accessToken })
-            .then((res) => {
-                if (Array.isArray(res.data)) {
-                    setVideos((prev) => [...prev, ...res.data]);
-                    setPage((prev) => prev + 1);
-                }
+    const loadMore = useCallback(() => {
+        return setTimeout(() => {
+            timelineService
+                .getVideos({ type: 'for-you', page: page, accessToken: accessToken })
+                .then((res) => {
+                    if (Array.isArray(res.data)) {
+                        setVideos((prev) => [...prev, ...res.data]);
+                        setPage((prev) => prev + 1);
+                    }
 
-                if (res.data.length === 0 || page === res.meta.pagination.total) {
-                    setNoMoreVideo(true);
-                }
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    }, [page, accessToken]);
+                    if (res.data.length === 0 || page === res.meta.pagination.total) {
+                        setNoMoreVideo(true);
+                    }
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        }, 300);
+    }, [page, accessToken, setVideos]);
+
+    useEffect(() => {
+        const timeout = loadMore();
+
+        return () => clearTimeout(timeout);
+    }, []);
 
     return (
         <div className={cx('wrapper')}>
